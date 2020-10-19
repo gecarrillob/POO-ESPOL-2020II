2ad70d13882e2db630b58231ad1396b8c5e191fe
Gladys Carrillo <gecarri@espol.edu.ec>
2020-10-19 15:49:41 -0500

    actualizacion de la clase cliente

--- Unidad1/POO-U1-ClasesObjetos/Cliente.java
+++ Unidad1/POO-U1-ClasesObjetos/Cliente.java
@@ -1,32 +1,48 @@
 public class Cliente{
   String nombre;
-  int porcDescuento;
+  int porcDescuento;//valor sobre 100
   boolean esAfiliado;
+
+  public Cliente(){
+
+  }
+  public Cliente(String n){
+    nombre = n;
+  }
+
+  public Cliente(String n, boolean a){
+    nombre = n;
+    esAfiliado =a;
+    if (a) porcDescuento=1;
+  }
 
   public void revisarMenu(){
     System.out.println("Usuario " + nombre + " revisa menu");
   }
 
   public boolean pagar(double valor){
+    if (porcDescuento>0)
+        valor=valor - (valor*porcDescuento/100);
     System.out.println("Usuario paga "+valor);
+    
     return true;
   }
 
   public static void main(String[] args) {
     System.out.println("Creando objetos:");
     //creando objetos
-    Cliente cliente1 = new Cliente();
-    Cliente cliente2 = new Cliente();
+    Cliente cliente1 = new Cliente("Luis");
+    Cliente cliente2 = new Cliente("Margarita",true);
     //datos para objetos
-    cliente1.nombre = "Luis";
+    //cliente1.nombre = "Luis";
     cliente1.porcDescuento = 2;
 
-    cliente2.nombre = "Margarita";
-    cliente2.esAfiliado = true;
+    //cliente2.nombre = "Margarita";
+    //cliente2.esAfiliado = true;
 
     //llamando a  metodos
     cliente1.revisarMenu();
-    cliente2.pagar(30.70);
+    cliente2.pagar(30.00);
 
     Cliente cliente3 = cliente2;
 
